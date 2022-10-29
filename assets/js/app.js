9dc32ee6a88478d028776639adb1ae0396c9457b
NoraA8 <zaske8@gmail.com>
2022-10-29 17:14:08 -0300

    prettier

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,21 +1,20 @@
-const amount = document.querySelector ("#amount");
-const price = document.querySelector ("#price");
-const btn = document.querySelector ("#btn");
-const color = document.querySelector ("#color");
-const priceTotal = document.querySelector ("#priceTotal");
-const amountTotal = document.querySelector ("#amountTotal");
-const buttomTotal = document.querySelector ("#buttomTotal");
+const amount = document.querySelector("#amount");
+const price = document.querySelector("#price");
+const btn = document.querySelector("#btn");
+const color = document.querySelector("#color");
+const priceTotal = document.querySelector("#priceTotal");
+const amountTotal = document.querySelector("#amountTotal");
+const buttomTotal = document.querySelector("#buttomTotal");
 
 /*Operacion de multiplicacion*/
 btn.addEventListener("click", () => {
-    amountTotal.textContent = "Cantidad: " + amount.value;
-    let price = 600000;
-    priceTotal.textContent = "Total: $" + price*amount.value;
+  amountTotal.textContent = "Cantidad: " + amount.value;
+  let price = 600000;
+  priceTotal.textContent = "Total: $" + price * amount.value;
 });
 
 /*Cambiando el atributo del elemento*/
-function cambiaColor (color){
- 
-      let buttomTotal = document.getElementById("buttomTotal");
-      buttomTotal.style.backgroundColor = color.value;
-    }
+function cambiaColor(color) {
+  let buttomTotal = document.getElementById("buttomTotal");
+  buttomTotal.style.backgroundColor = color.value;
+}
